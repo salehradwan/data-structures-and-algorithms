782c5fff33723b37bb078c6109fa3f4e26de1021
salehradwan <saleh-radwan@outlook.com>
2021-06-15 23:13:56 +0300

    Code Challenge 08: Regular Expressions, part 1

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -151,18 +151,7 @@ const listFoods = (recipe) => {
   }
   return result;
 };
-/*
-var indices = [];
-var array = ['a', 'b', 'a', 'c', 'a', 'd'];
-var element = 'a';
-var idx = array.indexOf(element);
-while (idx != -1) {
-  indices.push(idx);
-  idx = array.indexOf(element, idx + 1);
-}
-console.log(indices);
-// [0, 2, 4]
-*/
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
